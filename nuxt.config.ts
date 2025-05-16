8664a1b25b2234adb815cbeac8b5bdaef8eed728
mnenie <1alexpeshkov@gmail.com>
2025-05-16 12:53:55 +0300

    chore: init

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/icon',
+    '@nuxt/eslint'
+  ]
+})
