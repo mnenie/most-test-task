045de9783a5ffc75f49b67d64dc94125ff0f287e
mnenie <1alexpeshkov@gmail.com>
2025-05-17 00:30:55 +0300

    feat: add app config

---
+++ app/app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+  toaster: {
+    position: 'bottom-right' as const,
+    duration: 3000,
+  },
+  ui: {
+    colors: {
+      primary: 'orange',
+      neutral: 'slate',
+    },
+  },
+})
